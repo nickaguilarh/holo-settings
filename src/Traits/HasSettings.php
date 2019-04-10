0b2da04722723b3a734426e0887747b9f68bf88c
Nick Aguilar H <nickaguilarh@gmail.com>
2019-04-09 21:44:35 -0500

    add relationship to trait

--- src/Traits/HasSettings.php
+++ src/Traits/HasSettings.php
@@ -4,7 +4,17 @@
 namespace Holo\Traits;
 
 
+use Holo\EntitySetting;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+
 trait HasSettings
 {
-
+    /**
+     * Retrieves all the settings for this model.
+     * @return MorphMany
+     */
+    public function settings()
+    {
+        return $this->morphMany(EntitySetting::class, 'entity');
+    }
 }
